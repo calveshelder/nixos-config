0b92a5122b4fc6efdc5778e18c040b2c4758cbdd
Helder Gonçalves <goncalves.helder@proton.me>
2025-05-11 22:16:53 +0100

    fix: move age encrypted gpg file to more reasonable directory

--- modules/darwin/secrets.nix
+++ modules/darwin/secrets.nix
@@ -28,7 +28,7 @@ let user = "heldergoncalves"; in
 
   age.secrets."git-gpg-signing-key" = {
     symlink = false;
-    path = "/Users/${user}/.gnupg/private-keys-v1.d/gpg-signing-key.asc";
+    path = "/Users/${user}/.gnupg/gpg-signing-key.asc";
     file =  "${secrets}/git-gpg-signing-key.age";
     mode = "600";
     owner = "${user}";

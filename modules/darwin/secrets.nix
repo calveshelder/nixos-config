c0e18ada4988ce8ac35ea39d17dd3fe1fb26ed77
Helder Gonçalves <goncalves.helder@proton.me>
2025-05-11 23:08:21 +0100

    feat: new github key

--- modules/darwin/secrets.nix
+++ modules/darwin/secrets.nix
@@ -18,7 +18,7 @@ let user = "heldergoncalves"; in
 
   #
   age.secrets."github-ssh-key" = {
-    symlink = true;
+    symlink = false;
     path = "/Users/${user}/.ssh/github";
     file =  "${secrets}/github-ssh-key.age";
     mode = "600";

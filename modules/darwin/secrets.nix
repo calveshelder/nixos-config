1a82be5fce14c323efd8ec4a295426c48f2344ae
Helder Gonçalves <goncalves.helder@proton.me>
2025-05-11 22:26:02 +0100

    feat: temporarily remove the gpg key

--- modules/darwin/secrets.nix
+++ modules/darwin/secrets.nix
@@ -26,12 +26,4 @@ let user = "heldergoncalves"; in
   #   group = "staff";
   # };
 
-  age.secrets."git-gpg-signing-key" = {
-    symlink = false;
-    path = "/Users/${user}/.gnupg/git-gpg-signing-key.asc";
-    file =  "${secrets}/git-gpg-signing-key.age";
-    mode = "600";
-    owner = "${user}";
-  };
-
 }

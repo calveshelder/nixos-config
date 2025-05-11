4a398c40d7f1a504bbf170689977b494903e9057
Helder Gonçalves <goncalves.helder@proton.me>
2025-05-11 22:55:53 +0100

    feat: experimenting... confused with private/pub key settings

--- modules/shared/files.nix
+++ modules/shared/files.nix
@@ -1,7 +1,7 @@
 { pkgs, config, ... }:
 
 let
- githubPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDYH734HBgOlCKP3riSMaw/O1o/TkQsWif0oetdruuSi heldergoncalves@192.168.1.49";
+ githubPublicKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDySJ+KH6CUOxckJgzI1Ii9ue25OVxml3KrdCl31GU2xTG/vUC2Aq//psyHOmw/nCyhnqw2qUPFqgS32uc34vuWy+U2M7YqObHFyNw+kOtvhiL0uTXMpoxdw0pIulDVVSYJNHE4IVXQWwAjeZMuSyAOpoJL9dkhgWEY6TCS84id/W8FIJgWdVab+mIehgukQ1zRkjN8WgRUZ+Ks5YCi7TTVQvqcXJLlmG6YKcp2VTAuFKbiUdr72CrOF3bFGaECH6PyneB16eHdPgaH64SAqr27G8DfdRUZvRgNzDhMFR8d/y5RwEjJfLTbo0p7RvNETFBZkzlu2Fp8o/czHrV6O3he71NctPjCwYdbd4bYLo8sStF2D2+lqt+XDTxgRWxPgRdEv3S6mdegylMaU5yD5GcPntapu7OVBm5jgJWkoVFMuDCFOvs4qqLPp+zNL+MlV5HVlsR4fJ1sEjUzpURawDQCDV7uvsr1Stcrs1i0JXa2VyQ/rkx/TSiCuKMQWLWl8QU= heldergoncalves@192.168.1.21";
 in
 {
 

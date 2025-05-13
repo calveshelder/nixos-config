3227e90e3a2a15be26bb12fdcea146d82d1b6cf5
Helder Gonçalves <goncalves.helder@proton.me>
2025-05-13 23:52:12 +0100

    feat: devenv barebones template

--- lando-env/devenv.nix
+++ lando-env/devenv.nix
@@ -1,23 +1,45 @@
-{ pkgs, ... }:
+{ pkgs, lib, config, inputs, ... }:
+
 {
-  # Packages for Podman and Lando
-  packages = with pkgs; [
-    podman          # Container runtime
-    podman-compose  # For Docker Compose compatibility
-    nodejs_20       # Lando requires Node.js
-    curl
-    git
-  ];
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
 
-  # Environment variables for Podman and Lando
-  env = {
-    DOCKER_HOST = "unix:///run/user/$UID/podman/podman.sock";
-    LANDO_NO_DOCKER = "false";
-  };
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
 
-  # Ensure Podman socket starts
-  scripts.setup-podman.exec = ''
-    systemctl --user start podman.socket
-    systemctl --user enable podman.socket
+  # https://devenv.sh/languages/
+  # languages.rust.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.cargo-watch.exec = "cargo-watch";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = ''
+    echo hello from $GREET
   '';
+
+  enterShell = ''
+    hello
+    git --version
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
 }
